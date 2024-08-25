dba99db380640ad772e1d795279b3992069e9104
BermuGod28 <thejuanjo1128@gmail.com>
2024-08-25 14:00:24 -0500

    login funcionando

--- demo/src/main/java/com/example/demo/controlador/ClienteController.java
+++ demo/src/main/java/com/example/demo/controlador/ClienteController.java
@@ -1,7 +1,9 @@
 package com.example.demo.controlador;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
