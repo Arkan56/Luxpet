71b81c5e301c063de74fe9083b03162cb0afe775
Arkan56 <seb4s.s44@gmail.com>
2024-11-07 09:23:56 -0500

    Pequeñas modificaciones

--- demo/src/main/java/com/example/demo/servicio/VeterinarioServiceImpl.java
+++ demo/src/main/java/com/example/demo/servicio/VeterinarioServiceImpl.java
@@ -42,8 +42,8 @@ public class VeterinarioServiceImpl implements VeterinarioService{
     }
 
     @Override
-    public void add(Veterinario veterinario) {
-        veterinarioRepository.save(veterinario);
+    public Veterinario add(Veterinario veterinario) {
+        return veterinarioRepository.save(veterinario);
     }
 
     @Override
